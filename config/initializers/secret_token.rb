9ea6c44bbff1f05155edaced07ac1710fc7ce4a6
Craig Ambrose <craig@craigambrose.com>
2012-02-05 15:55:58 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Permapedia::Application.config.secret_token = '69decdab1db8834f364f6d2dddb68ba9504b1c100559bc70e21877564b476336a34139f9e23424a31401b6705a01819213b77039abd08c10f304645a530f77a8'
