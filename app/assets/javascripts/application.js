8ffd64fac23d4957cc0b05878dff5add697c59f1
Craig Ambrose <craig@craigambrose.com>
2012-02-05 19:13:58 +1300

    added responsiveness to bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,23 +5,35 @@
 // the compiled file.
 //
 //= require jquery
-//= require jquery_ujs
-//= require jquery-ui
-//= require jquery.livequery
-//= require cocoon
-//= require autocomplete-rails
+//= XXrequire jquery_ujs
+//= XXrequire jquery-ui
+//= XXrequire jquery.livequery
+//= XXrequire cocoon
+//= XXrequire autocomplete-rails
 //= require twitter/bootstrap
-//= require combo/jquery.eventdelay.js
-//= require combo/jquery.combo.js
-//= require_tree .
+//= require twitter/bootstrap/bootstrap-alert
+//= require twitter/bootstrap/bootstrap-button
+//= require twitter/bootstrap/bootstrap-carousel
+//= require twitter/bootstrap/bootstrap-collapse
+//= require twitter/bootstrap/bootstrap-dropdown
+//= require twitter/bootstrap/bootstrap-modal
+//= require twitter/bootstrap/bootstrap-popover
+//= require twitter/bootstrap/bootstrap-scrollspy
+//= require twitter/bootstrap/bootstrap-tab
+//= require twitter/bootstrap/bootstrap-tooltip
+//= require twitter/bootstrap/bootstrap-transition
+//= require twitter/bootstrap/bootstrap-typeahead
+//= XXrequire combo/jquery.eventdelay.js
+//= XXrequire combo/jquery.combo.js
+//= XXrequire_tree .
 //
 
-$.datepicker.setDefaults({
-  dateFormat: 'yy-mm-dd'
-});
-$("input.date").datepicker();
-
-
-$("select.combo").livequery(function (){
-  $(this).combo({triggerSelected:true});
-});
+// $.datepicker.setDefaults({
+//   dateFormat: 'yy-mm-dd'
+// });
+// $("input.date").datepicker();
+// 
+// 
+// $("select.combo").livequery(function (){
+//   $(this).combo({triggerSelected:true});
+// });
